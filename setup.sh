bbd10378278cd44876ac0181fff7021394aef585
kartik <kartik.ghorai@b4b.ai>
2025-05-05 10:36:29 +0530

    Update requirements and setup scripts for consistency and version control

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
 # Install PyTorch
-pip install torch
+pip install torch==2.6.0
 
 # Install wheel, packaging, and ninja
-pip install wheel packaging ninja
+pip install packaging==24.2 ninja==1.11.1.3
 
 # Install flash-attn and deepspeed
-pip install flash-attn deepspeed
+pip install flash-attn==2.7.4.post1 --no-build-isolation
+pip install  deepspeed==0.16.3
 
 # Install requirements from requirements.txt
 pip install -r requirements.txt
