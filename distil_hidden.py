336dd0d0477d0a6a9257fd38cc0ca586e36e05f4
Sid <sidchaudhary04@gmail.com>
2025-08-12 23:47:36 +0100

    tokeniser testing

--- distil_hidden.py
+++ distil_hidden.py
@@ -13,7 +13,7 @@ config = {
     "dataset": {
         "name": "mlabonne/FineTome-100k",
         "split": "train",
-        # "num_samples": 100000, # You can pass a number here to limit the number of samples to use.
+        "num_samples": 10, # You can pass a number here to limit the number of samples to use.
         "seed": 42
     },
     "models": {
@@ -234,10 +234,8 @@ class CustomSFTTrainer(SFTTrainer):
         return total_loss
 
 # Training arguments
-# Training arguments
 training_arguments = TrainingArguments(
     **config["training"],
-    remove_unused_columns=False,
 )
 
 # Create the custom SFT Trainer
