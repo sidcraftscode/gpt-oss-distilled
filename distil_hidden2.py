62d51376064dfee0e9f22661bef5ee5601d285aa
Sid <sidchaudhary04@gmail.com>
2025-08-13 23:48:16 +0100

    save_steps

--- distil_hidden2.py
+++ distil_hidden2.py
@@ -79,7 +79,7 @@ config = {
         "num_train_epochs": 1,
         "per_device_train_batch_size": 2,
         "gradient_accumulation_steps": 16,
-        "save_steps": 100,
+        "save_steps": 1000,
         "logging_steps": 2,
         "save_total_limit": 2,
         "learning_rate": 1e-5,
